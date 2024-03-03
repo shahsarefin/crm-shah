846de111daef1c4a45b55ce843ce28ff10ca3625
shahsarefin <shahsarefin@gmail.com>
2024-03-03 17:54:29 -0600

    Controller Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/alphabetized'
+  get 'customers/missing_email'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
